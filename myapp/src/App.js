da7a00ff7b25e1954f0b2f002cd25a07ceb25ffb
MuhammadAmeen321 <alirazajunejo38@gmail.com>
2026-02-24 17:48:49 +0500

    This is props commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
 
-import Component from "./component";
-import MyComponent from './click';
+// import Component from "./component";
+// import MyComponent from './click';
+import Classprops from './classprops';
 
 function App() {
   return (
     <div>
     <h1>Hello welcome to react practice</h1>
-    <Component/>
-    <MyComponent/>
+    {/* <Component/>
+    <MyComponent/> */}
+    <Classprops name='1'/>
+    <Classprops name='2'/>
     </div>
   );
 }
---
+++ myapp/src/classprops.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Classprops extends Component{
+
+    render(){
+        return <div> 
+            <p> Hello this is props {this.props.name}
+            </p>
+            
+        </div>
+    }
+}
+
+
+
+
+export default Classprops
