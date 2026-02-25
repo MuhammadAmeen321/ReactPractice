92ec4e798c5ff61a9e83f8afbfa7cb7c82e3c4bc
MuhammadAmeen321 <alirazajunejo38@gmail.com>
2026-02-25 23:03:03 +0500

    added styling in classprops

--- myapp/src/classprops.js
+++ myapp/src/classprops.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import App from './App.css';
 
 class Classprops extends Component{
 
     render(){
-        return <div> 
+        return <div className="App"> 
             <p> Hello this is props {this.props.name}
             </p>
+
             
         </div>
     }
